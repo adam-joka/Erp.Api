c89ba3ee7d3b3b92d7e5c3d69fcfde4cb8b01239
Adam Joka <adam.joka@gmail.com>
2024-01-25 14:04:07 +0100

    fix swagger

--- src/Erp.Api/Program.cs
+++ src/Erp.Api/Program.cs
@@ -4,14 +4,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTrucksModule();
 
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseSwagger();
-
 app.UseTrucksModuleEndpoints();
 
+app.MapSwagger();
 app.UseSwaggerUI();
 
 app.Run();
