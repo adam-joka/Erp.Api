d10043562f19f3ebfa5aad4534f7269b4c591a15
Adam Joka <adam.joka@gmail.com>
2024-01-30 08:58:22 +0100

    add employees module

--- src/Erp.Api/Program.cs
+++ src/Erp.Api/Program.cs
@@ -1,15 +1,23 @@
+using Erp.Employees;
 using Erp.Trucks;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// modules start
 builder.Services.AddTrucksModule();
+builder.Services.AddEmployeesModule();
+
+// modules end
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// modules start
 app.UseTrucksModuleEndpoints();
+app.UseEmployeesModuleEndpoints();
+// modules end
 
 app.MapSwagger();
 app.UseSwaggerUI();
---
+++ src/Erp.Employees/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Erp.Employees;
+
+public static class Extensions
+{
+    public static void AddEmployeesModule(this IServiceCollection services)
+    {
+        
+    }
+    
+    public static void UseEmployeesModuleEndpoints(this WebApplication app)
+    {
+    }
+}
